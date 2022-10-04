6d9b20d02d95f39a1af7757f4cf6fc8931408498
Ramkumar1108 <ram_k@nakayamairon.co.jp>
2022-10-04 16:42:50 +0900

    memo
    memo

---
+++ fundamentalsOOPS.cpp
@@ -0,0 +1,11 @@
+/*
+    Abstraction              : To simplify reality.
+    Encapsulation            : Hiding data and complexity.
+    Inheritance              : A class can derive its method and properties from another class.
+    Polymorphism(many forms) : A class can implement an inherited method in its own way.
+*/
+
+/*
+    List
+    Disctionary
+*/
